fd12db014e559d2fe540be1a58c2df8dc2be5236
technovahub@2025 <technovahubcareer@gmail.com>
2026-02-10 11:20:18 +0530

    Main.jsx file is updated

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,27 +1,20 @@
-import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import Sidebar from '../components/Sidebar';
-import Header from '../components/Header';
-import './MainLayout.css';
+import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
+import { Outlet } from "react-router-dom";
+import "./MainLayout.css";
 
-const MainLayout = ({ children }) => {
-    const [expandedPanel, setExpandedPanel] = useState(null);
-    const location = useLocation();
-
-    // Only show header on broadcast page
-    const shouldShowHeader = location.pathname === '/';
-
-    return (
-        <div className="main-layout">
-            <Sidebar expandedPanel={expandedPanel} setExpandedPanel={setExpandedPanel} />
-            <div className={`main-content ${expandedPanel ? 'content-shifted' : ''}`}>
-                {shouldShowHeader && <Header />}
-                <main className="page-content">
-                    {children}
-                </main>
-            </div>
-        </div>
-    );
+const MainLayout = () => {
+  return (
+    <div className="main-layout">
+      <Sidebar />
+      <div className="main-content">
+        <Header />
+        <main className="page-content">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default MainLayout;
