96e264e4995cc3d535d6d3cc6d75bab8f2eedaec
technovahub@2025 <technovahubcareer@gmail.com>
2026-03-23 10:41:42 +0530

    add Eye and EyeOff icons for improved user interface in admin page

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import socketService from "../services/socketService";
-import { ArrowUpRight, BadgeCheck, CreditCard, Fingerprint, Settings2, Shield, ShieldCheck, UsersRound, WalletCards } from "lucide-react";
+import { ArrowUpRight, BadgeCheck, CreditCard, Eye, EyeOff, Fingerprint, Settings2, Shield, ShieldCheck, UsersRound, WalletCards } from "lucide-react";
 import "./admin.css";
 import "../styles/theme.css";
 
